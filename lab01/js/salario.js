305f1ffee1971c7e76b727a20f70efaf79b8b71c
Roger Ulate <roger.p.designe357@gmail.com>
2021-02-10 00:25:55 -0600

    Avance 1

---
+++ lab01/js/salario.js
@@ -0,0 +1,12 @@
+(function() {
+
+	let salarioPorHoras = 0;
+	let diasTrabajados = 0;
+	let porcentajeDeducciones = 0;
+	let salarioBruto = 0;
+	let deducciones = 0;
+	let salarioNeto = 0;
+
+	
+
+})()
